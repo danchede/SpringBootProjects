0917ffaa65f87d22a875c05c9bfa2bbcc8b6c3a9
danchede <1320030382@qq.com>
2018-05-10 13:31:18 +0800

    SpringBoot使用SpringSecurity搭建基本环境完成

---
+++ springbootspringsecurity/src/main/java/com/ifox/springbootspringsecurity/SpringbootspringsecurityApplication.java
@@ -0,0 +1,12 @@
+package com.ifox.springbootspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootspringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootspringsecurityApplication.class, args);
+	}
+}
